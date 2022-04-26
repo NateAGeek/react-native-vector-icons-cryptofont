2be526a9857d09f09639b24fc482dd3cce668ca2
SpaceBoiNate <nathaniel@astroglu.com>
2022-04-25 23:12:32 -0500

    Init commit

---
+++ CryptoFont.ts
@@ -0,0 +1,4 @@
+import { createIconSet } from 'react-native-vector-icons';
+import glyphMap from './cryptofont.glyphmap.json';
+const CryptoFontIcon = createIconSet(glyphMap, 'CryptoFont', 'cryptofont.ttf');
+export default CryptoFontIcon;
